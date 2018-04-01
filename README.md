b42d0853a061c9c8b960474bae6294de170b1773
i.ivanina <ilya.ivanina@gmail.com>
2018-04-02 00:34:31 +0300

    init and try. Did not work out.

---
+++ README.md
@@ -0,0 +1,5 @@
+
+####Source:
+https://docs.spring.io/spring-shell/docs/1.2.x-SNAPSHOT/reference/htmlsingle/#simple-application
+
+Did not work out.
---
+++ src/main/java/demo/Apication.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.shell.Bootstrap;
+
+import java.io.IOException;
+
+public class Apication {
+    /*public static void main(String[] args) throws IOException {
+        //Bootstrap.main(args);
+    }*/
+}
